e345f4ea421f8748e3e59fa1d0165acedcf92a35
msddragon0 <mateus.schneider.msd@gmail.com>
2026-09-15 21:58:58 -0300

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+getgenv().DRONX = {
+    Enabled       = true,
+    AutoFarm      = false,
+    AutoMaestria  = false,
+    AutoHeal      = false,
+    AutoQuest     = false,
+    AutoChest     = false,
+    AntiAFK       = true,
+    TipoArma      = "Auto",
+    MaxDistance   = 2000,
+    HealThreshold = 0.5,
+    Kills         = 0,
+}
